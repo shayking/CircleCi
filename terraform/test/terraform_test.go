d7e815c84bc2e1a14b60c8b69be33444ac505408
shay.sardam <shaysss8734@gmail.com>
2024-05-02 17:44:45 +0300

    sadasdsdsadsa

--- terraform/test/terraform_test.go
+++ terraform/test/terraform_test.go
@@ -1,39 +1,31 @@
 package test
 
 import (
-    "os/exec"
-    "testing"
-
-    "github.com/gruntwork-io/terratest/modules/terraform"
+	"os/exec"
+	"testing"
 )
 
-func TestTerraformValidation(t *testing.T) {
-    terraformOptions := &terraform.Options{
-        // Path to the Terraform code directory
-        TerraformDir: "..",
-    }
+func TestTerraform(t *testing.T) {
+	t.Parallel()
 
-    t.Parallel()
+	t.Run("Init", func(t *testing.T) {
+		out, err := exec.Command("terraform", "init").CombinedOutput()
+		if err != nil {
+			t.Errorf("Error running terraform init: %v\nOutput:\n%s", err, string(out))
+		}
+	})
 
-    t.Run("Init", func(t *testing.T) {
-        out, err := exec.Command("terraform", "init").CombinedOutput()
-        if err != nil {
-            t.Errorf("Error running terraform init: %v\nOutput:\n%s", err, string(out))
-        }
-    })
+	t.Run("Validate", func(t *testing.T) {
+		out, err := exec.Command("terraform", "validate").CombinedOutput()
+		if err != nil {
+			t.Errorf("Error running terraform validate: %v\nOutput:\n%s", err, string(out))
+		}
+	})
 
-    t.Run("Validate", func(t *testing.T) {
-        out, err := exec.Command("terraform", "validate").CombinedOutput()
-        if err != nil {
-            t.Errorf("Error running terraform validate: %v\nOutput:\n%s", err, string(out))
-        }
-    })
-
-    t.Run("Plan", func(t *testing.T) {
-        out, err := exec.Command("terraform", "plan").CombinedOutput()
-        if err != nil {
-            t.Errorf("Error running terraform plan: %v\nOutput:\n%s", err, string(out))
-        }
-    })
-
+	t.Run("Plan", func(t *testing.T) {
+		out, err := exec.Command("terraform", "plan").CombinedOutput()
+		if err != nil {
+			t.Errorf("Error running terraform plan: %v\nOutput:\n%s", err, string(out))
+		}
+	})
 }
