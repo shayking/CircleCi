65c131a8c1fe344a45ff16da02565a3b5747f347
shay.sardam <shaysss8734@gmail.com>
2024-05-02 17:46:01 +0300

    dsadasddas

--- terraform/test/terraform_test.go
+++ terraform/test/terraform_test.go
@@ -8,24 +8,24 @@ import (
 func TestTerraform(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Init", func(t *testing.T) {
-		out, err := exec.Command("terraform", "init").CombinedOutput()
-		if err != nil {
-			t.Errorf("Error running terraform init: %v\nOutput:\n%s", err, string(out))
-		}
-	})
+	cmd := exec.Command("terraform", "init")
+	cmd.Dir = ".." // Assuming the test directory is one level inside the Terraform directory
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("Error running terraform init: %v\nOutput:\n%s", err, string(out))
+	}
 
-	t.Run("Validate", func(t *testing.T) {
-		out, err := exec.Command("terraform", "validate").CombinedOutput()
-		if err != nil {
-			t.Errorf("Error running terraform validate: %v\nOutput:\n%s", err, string(out))
-		}
-	})
+	cmd = exec.Command("terraform", "validate")
+	cmd.Dir = ".."
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("Error running terraform validate: %v\nOutput:\n%s", err, string(out))
+	}
 
-	t.Run("Plan", func(t *testing.T) {
-		out, err := exec.Command("terraform", "plan").CombinedOutput()
-		if err != nil {
-			t.Errorf("Error running terraform plan: %v\nOutput:\n%s", err, string(out))
-		}
-	})
+	cmd = exec.Command("terraform", "plan")
+	cmd.Dir = ".."
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		t.Errorf("Error running terraform plan: %v\nOutput:\n%s", err, string(out))
+	}
 }
